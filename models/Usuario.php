112b41ee45f9e7dbd0cf4f6191e06fd0d72d43a9
GustavoMessiasDeAzevedo <134647938+GustavoMessiasDeAzevedo@users.noreply.github.com>
2023-09-01 17:04:17 -0300

    acrescentando a conexão no bano de dados update e insert

--- models/Usuario.php
+++ models/Usuario.php
@@ -30,6 +30,28 @@ class Usuario{
 
     */
     public function cadastrar($dados){
+        try{
+            $query = "INSERT INTO {$this->table}(nome, email, senha, perfil) VALUES (:nome, :email, :senha, :perfil)";
+
+            $stmt = $this->db->prepare($query);
+
+
+            
+
+        }catch(PDOException $e){
+            echo "Erro na preparação da consulta: ". $e->getMessage();
+        }
+            $stmt->bindParam(':nome', $dados['nome']);
+            $stmt->bindParam(':email', $dados['email']);
+            $stmt->bindParam(':senha', $dados['senha']);
+            $stmt->bindParam(':perfil', $dados['perfil']);
+
+        try{
+            $stmt->execute();
+            echo "Inserção bem-sucedida!";
+        }catch(PDOException $e){
+            echo "Erro na inserção: ". $e->getMessage();
+        }
 
     }
 
@@ -40,6 +62,28 @@ class Usuario{
 
     */
     public function editar($id, $dados){
+
+        try{
+            $query = "UPDATE {$this->table} SET nome = :nome ,email = :email, senha = :senha, perfil = :perfil WHERE id_usuario = :id_usuario";
+
+            $stmt = $this->db->prepare($query);
+
+        }catch(PDOException $e){
+            echo "Erro de preparação de consulta: ". $e->getMessage();
+        }
+
+            $stmt->bindParam(':nome', $dados['nome']);
+            $stmt->bindParam(':email', $dados['email']);
+            $stmt->bindParam(':senha', $dados['senha']);
+            $stmt->bindParam(':perfil', $dados['perfil']);
+
+            try{
+                $stmt->execute();
+                echo "Seus dados foram atualizados com sucesso!";
+            }catch(PDOException $e){
+                echo "Erro na inserção: ". $e->getMessage();
+            }
+
 
     }
     
